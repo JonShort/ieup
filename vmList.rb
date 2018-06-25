80e2079dcf107d9d5ea8481194812132588da7ca
Jon Short <JonShort@users.noreply.github.com>
2018-06-25 10:19:17 +0100

    - Updated the VMList object to include the correct key/value pairs for the new URLs - Updated the Vagrantfile, comments show the new URL shape

--- vmList.rb
+++ vmList.rb
@@ -1,15 +1,15 @@
 module VMList
   # Box names, keys are what user will add to env var, literal is used for download link
   BOXES ||= {
-    "xp-ie6" => "vagrant-xp-ie6",
-    "xp-ie8" => "vagrant-xp-ie8",
-    "vista-ie7" => "vagrant-vista-ie7",
-    "win7-ie8" => "vagrant-win7-ie8",
-    "win7-ie9" => "vagrant-win7-ie9",
-    "win7-ie10" => "vagrant-win7-ie10",
-    "win7-ie11" => "vagrant-win7-ie11",
-    "win8-ie10" => "vagrant-win8-ie10",
-    "win81-ie11" => "vagrant-win81-ie11",
-    "win10-msedge" => "vagrant-win10-msedge",
+    "xp-ie6" => "ie6.xp.vagrant",
+    "xp-ie8" => "ie8.xp.vagrant",
+    "vista-ie7" => "ie7.vista.vagrant",
+    "win7-ie8" => "ie8.win7.vagrant",
+    "win7-ie9" => "ie9.win7.vagrant",
+    "win7-ie10" => "ie10.win7.vagrant",
+    "win7-ie11" => "ie11.win7.vagrant",
+    "win8-ie10" => "ie10.win8.vagrant",
+    "win81-ie11" => "ie11.win81.vagrant",
+    "win10-msedge" => "msedge.win10.vagrant",
   }
 end
